20133753ddb948d0997104f7ed28372a5c5c8023
Ronyx-Dev <44080341+Ronyx-Dev@users.noreply.github.com>
2024-06-27 20:04:55 -0500

    Update sensor.py

--- custom_components/clyrpool/sensor.py
+++ custom_components/clyrpool/sensor.py
@@ -1,6 +1,5 @@
 import logging
 from datetime import timedelta
-import time
 
 import voluptuous as vol
 from selenium import webdriver
@@ -11,44 +10,52 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
-from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, UnitOfTemperature
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Clyrpool Water Quality'
+CONF_EMAIL = "email"
+CONF_PASSWORD = "password"
+CONF_URL = "url"
+
+DEFAULT_NAME = "Clyrpool Monitor"
 SCAN_INTERVAL = timedelta(minutes=30)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
+    vol.Required(CONF_URL): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the sensor platform."""
+    """Set up the Clyrpool sensor."""
     email = config[CONF_EMAIL]
     password = config[CONF_PASSWORD]
+    url = config[CONF_URL]
+    name = config.get(CONF_NAME)
 
-    add_entities([ClyrpoolWaterQualitySensor(email, password)], True)
+    add_entities([ClyrpoolSensor(name, email, password, url)], True)
 
-class ClyrpoolWaterQualitySensor(SensorEntity):
-    """Representation of a Sensor."""
+class ClyrpoolSensor(Entity):
+    """Representation of a Clyrpool sensor."""
 
-    def __init__(self, email, password):
+    def __init__(self, name, email, password, url):
         """Initialize the sensor."""
-        self._state = None
+        self._name = name
         self._email = email
         self._password = password
-        self._ph = None
-        self._orp = None
-        self._water_level = None
-        self._water_temp = None
+        self._url = url
+        self._state = None
+        self._attributes = {}
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return DEFAULT_NAME
+        return self._name
 
     @property
     def state(self):
@@ -58,87 +65,53 @@ class ClyrpoolWaterQualitySensor(SensorEntity):
     @property
     def extra_state_attributes(self):
         """Return the state attributes."""
-        return {
-            'ph': self._ph,
-            'orp': self._orp,
-            'water_level': self._water_level,
-            'water_temp': self._water_temp,
-        }
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return UnitOfTemperature.CELSIUS
+        return self._attributes
 
     def update(self):
         """Fetch new state data for the sensor."""
+        options = Options()
+        options.headless = True
+        options.add_argument("--window-size=1920,1080")
+
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
+
         try:
-            # Set up Chrome options
-            options = Options()
-            options.headless = True  # Run in headless mode
-            options.add_argument("--window-size=1920,1080")  # Optional: Set the window size
-
-            # Set up the WebDriver
-            service = Service(ChromeDriverManager().install())
-            driver = webdriver.Chrome(service=service, options=options)
-
-            # Navigate to the webpage
-            url = "https://app.clyrpool.com/dashboard/location/RyiIrtevakEc2yJZSRaz/waterQuality/maintenance/"
-            driver.get(url)
-
-            # Add explicit wait to ensure the page loads completely
+            driver.get(self._url)
             wait = WebDriverWait(driver, 10)
 
-            # Locate the email input field by its ID and enter the email address
-            _LOGGER.debug("Locating email input field")
             email_input = wait.until(EC.presence_of_element_located((By.ID, "Email Address-input")))
             email_input.clear()
             email_input.send_keys(self._email)
-            _LOGGER.debug("Email address entered successfully")
 
-            # Locate the password input field by its ID and enter the password
-            _LOGGER.debug("Locating password input field")
             password_input = wait.until(EC.presence_of_element_located((By.ID, "Password-input")))
             password_input.clear()
             password_input.send_keys(self._password)
-            _LOGGER.debug("Password entered successfully")
 
-            # Locate the submit button and click it
-            _LOGGER.debug("Locating submit button")
             submit_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
             submit_button.click()
-            _LOGGER.debug("Form submitted successfully")
 
-            # Wait for the login to complete and then reload the page
-            time.sleep(5)  # Wait for a short period to ensure login completes
-            driver.get(url)  # Reload the page to ensure the correct page is displayed
-            _LOGGER.debug("Page reloaded after login")
+            # Add a wait to ensure the next page loads
+            wait.until(EC.url_changes(self._url))
+            time.sleep(10)  # Adjust the sleep time if needed
 
-            # Wait for the page to load completely after reload
-            wait.until(EC.presence_of_element_located((By.XPATH, "//p[text()='pH']")))
+            ph_value = driver.find_element(By.XPATH, "//p[text()='pH']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='pH')]").text
+            orp_value = driver.find_element(By.XPATH, "//p[text()='ORP']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='ORP')]").text
+            water_level_value = driver.find_element(By.XPATH, "//p[text()='Water Level']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='Water Level')]").text
+            water_temp_value = driver.find_element(By.XPATH, "//p[text()='Water Temp']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='Water Temp')]").text
 
-            # Extract the water quality data
-            _LOGGER.debug("Extracting water quality data")
-            self._ph = self.extract_value(driver, "//p[text()='pH']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='pH')]", "pH")
-            self._orp = self.extract_value(driver, "//p[text()='ORP']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='ORP')]", "ORP")
-            self._water_level = self.extract_value(driver, "//p[text()='Water Level']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='Water Level')]", "Water Level")
-            self._water_temp = self.extract_value(driver, "//p[text()='Water Temp']/ancestor::div[contains(@class, 'MuiPaper-root')]/descendant::p[contains(@class, 'MuiTypography-body1') and not(text()='Water Temp')]", "Water Temp")
+            self._state = "Online"
+            self._attributes = {
+                "pH": ph_value,
+                "ORP": orp_value,
+                "Water Level": water_level_value,
+                "Water Temperature": water_temp_value
+            }
 
-            self._state = "OK"
+        except Exception as e:
+            _LOGGER.error("An error occurred while updating the sensor: %s", e)
+            self._state = "Error"
+            self._attributes = {}
+
+        finally:
             driver.quit()
-
-        except Exception as e:
-            _LOGGER.error(f"An error occurred while updating the sensor: {e}")
-            self._state = None
-
-    def extract_value(self, driver, xpath, name):
-        """Helper function to extract value from the web page."""
-        try:
-            element = driver.find_element(By.XPATH, xpath)
-            value = element.text if element else None
-            _LOGGER.debug(f"{name} element found: {element}")
-            _LOGGER.debug(f"{name} value: {value}")
-            return value
-        except Exception as e:
-            _LOGGER.error(f"Error extracting {name} value: {e}")
-            return None
